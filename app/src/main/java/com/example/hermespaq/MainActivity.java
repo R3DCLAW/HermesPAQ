0f2705a8054b1cc50374e897cd528913c9e5a167
Ruben Frias <rubenfrias10@gmail.com>
2019-05-30 11:37:40 -0400

    Pantalla login completa

--- app/src/main/java/com/example/hermespaq/MainActivity.java
+++ app/src/main/java/com/example/hermespaq/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.hermespaq;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +21,21 @@ public class MainActivity extends AppCompatActivity {
         Name = (EditText) findViewById(R.id.etName);
         Password = (EditText) findViewById(R.id.etPassword);
         Login = (Button) findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        })
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName.equals("Admin"))&& (userPassword.equals("holamundo"))){
+            Intent intent = new Intent(MainActivity.this, Customers.class);
+            startActivity(intent);
+        }else{
+            Toast.makeText(getApplicationContext(),"Wrong Information!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
