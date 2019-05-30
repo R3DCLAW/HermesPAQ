caaa52194ee16e80cfb730e08feaa07c90c35cea
Ruben Frias <rubenfrias10@gmail.com>
2019-05-30 10:52:15 -0400

    Progreso en pantalla login

---
+++ app/src/main/java/com/example/hermespaq/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.hermespaq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name, Password;
+    private Button Login, Clear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+    }
+}
