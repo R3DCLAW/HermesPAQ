4b1ccc30fedf87c901704db03ff67844cbb29103
javier1201 <jose_1201@live.com>
2019-06-03 18:37:34 -0400

    Third Activity Completed

--- app/src/main/java/com/example/hermespaq/MainActivity.java
+++ app/src/main/java/com/example/hermespaq/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         Name = (EditText) findViewById(R.id.etName);
         Password = (EditText) findViewById(R.id.etPassword);
         Login = (Button) findViewById(R.id.btnLogin);
-        Clear = (Button) findViewById(R.id.btnClear);
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,19 +28,11 @@ public class MainActivity extends AppCompatActivity {
                 validate(Name.getText().toString(), Password.getText().toString());
             }
         });
-
-        Clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Password.setText("");
-                Name.setText("");
-            }
-        });
     }
 
     private void validate(String userName, String userPassword){
-        if((userName.equals("Admin")) && (userPassword.equals("holamundo"))){
-            Intent intent = new Intent(getApplicationContext(), Customers.class);
+        if((userName.equals("Admin"))&& (userPassword.equals("holamundo"))){
+            Intent intent = new Intent(MainActivity.this, Customers.class);
             startActivity(intent);
         }else{
             Toast.makeText(getApplicationContext(),"Wrong Information!", Toast.LENGTH_SHORT).show();
