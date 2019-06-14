7bdfcb0f8ddee737efffbcce11b2190948fcab83
Javier <jose_1201@live.com>
2019-06-14 16:32:42 -0400

    Application complete

--- app/src/main/java/com/example/hermespaq/Customers.java
+++ app/src/main/java/com/example/hermespaq/Customers.java
@@ -1,58 +1,54 @@
 package com.example.hermespaq;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class Customers extends AppCompatActivity {
-    int [] IMAGES = {R.drawable.hola1, R.drawable.hola2, R.drawable.hola3, R.drawable.hola4};
-    String [] NAMES = {"Antonio Galvan", "Julio Castro", "Maria Mejia", "Laura Pineda"};
-    String [] DESCRIPTIONS = {"2 paquetes", "1 paquete", "3 paquetes", "7 paquetes"};
+    ListView lista;
 
+    String [][] datos = {
+            {"Antonio Galvan", "2 paquetes", "2", "10lb", "antoniogalvan@gmail.com", "809-555-4555", "05/06/1986", "Piantini", "3"},
+            {"Julio Castro", "1 paquete", "1", "5lb", "j.castro@hotmail.com", "809-750-8888", "03/05/1990", "Naco", "6"},
+            {"Maria Mejia", "3 paquetes", "3", "17lb", "mar.m@yahoo.es", "809-732-3434", "0|/02/1995", "Arroyo Hondo", "8"},
+            {"Laura Pineda", "7 paquetes", "7", "30lb", "l.pineda@gmail.com", "829-330-5656", "05/12/1999", "Bella Vista", "20"},
+    };
+    int [] IMAGES = {R.drawable.hola1, R.drawable.hola2, R.drawable.hola3, R.drawable.hola4};
+    Double [] coordenada1 = {18.474603, 18.475163, 18.499419, 18.450046};
+    double [] coordenada2 = {-69.928120, -69.930050, -69.970684, -69.944756};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customers);
 
-        ListView listView=(ListView)findViewById(R.id.listView);
-        CustomAdapter customAdapter=new CustomAdapter();
+        lista = (ListView) findViewById(R.id.listView);
 
-        listView.setAdapter(customAdapter);
-    }
+        lista.setAdapter(new Adaptador(this, datos, IMAGES));
 
-    class CustomAdapter extends BaseAdapter{
-
-        @Override
-        public int getCount() {
-            return IMAGES.length;
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return 0;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            view = getLayoutInflater().inflate(R.layout.customlayout, null);
-            ImageView imageView=(ImageView)view.findViewById(R.id.imageView2);
-            TextView textView_name =(TextView)view.findViewById(R.id.textView_name);
-            TextView textView_description =(TextView)view.findViewById(R.id.textView_description);
-            imageView.setImageResource(IMAGES[i]);
-            textView_name.setText(NAMES[i]);
-            textView_description.setText(DESCRIPTIONS[i]);
-
-            return view;
-        }
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent visorDetalles = new Intent(view.getContext(), ProfileActivity.class);
+                visorDetalles.putExtra("NOM", datos[position][0]);
+                visorDetalles.putExtra("REC", datos[position][8]);
+                visorDetalles.putExtra("DISP", datos[position][2]);
+                visorDetalles.putExtra("PES", datos[position][3]);
+                visorDetalles.putExtra("CORR", datos[position][4]);
+                visorDetalles.putExtra("TEL", datos[position][5]);
+                visorDetalles.putExtra("FEC", datos[position][6]);
+                visorDetalles.putExtra("DIR", datos[position][7]);
+                visorDetalles.putExtra("Imagen", IMAGES[position]);
+                visorDetalles.putExtra("latitud", coordenada1[position]);
+                visorDetalles.putExtra("longitud", coordenada2[position]);
+                startActivity(visorDetalles);
+            }
+        });
     }
 }
